814c12bf65da8e34218b4cb14151e3ddd9c77d71
Bronuh <bronyx@yandex.ru>
2024-09-18 11:12:37 +0300

    Add urls storage

---
+++ Launcher/src/Launcher/Urls.cs
@@ -0,0 +1,22 @@
+﻿namespace Launcher;
+
+public static class Urls
+{
+    public const string BaseUrl = "https://minecraft.bronuh.ru/api/";
+    public const string JreUrl = BaseUrl + "core/jre/";
+    public const string MinecraftUrl = BaseUrl + "core/minecraft/";
+    
+    public const string ModsUrl = BaseUrl + "directory/mods/list/";
+    public const string FileDownloadUrl = BaseUrl + "directory/download/";
+    public const string ModsDownloadBaseUrl = BaseUrl + "directory/download/snapshots/current/";
+
+    public static string GetModDownloadUrl(string modName)
+    {
+        return $"{ModsDownloadBaseUrl}{modName}";
+    }
+
+    public static string GetFileDownloadUrl(string path)
+    {
+        return $"{FileDownloadUrl}{path}";
+    }
+}
