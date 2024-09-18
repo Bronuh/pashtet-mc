3d3ec8eb08d9833e8e54f9ee00c18453c135e73c
Bronuh <bronyx@yandex.ru>
2024-09-18 11:13:01 +0300

    Add HardLinkHelper

---
+++ Launcher/src/IO/HardLinkHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+
+namespace IO;
+
+public static class HardLinkHelper
+{
+    public static void DeleteHardLink(string linkPath)
+    {
+        if (File.Exists(linkPath))
+        {
+            File.Delete(linkPath);
+            Console.WriteLine($"Hard link '{linkPath}' has been deleted.");
+        }
+        else
+        {
+            Console.WriteLine($"File '{linkPath}' does not exist.");
+        }
+    }
+
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern bool CreateHardLink(string lpFileName, string lpExistingFileName, IntPtr lpSecurityAttributes);
+
+    public static void CreateHardLink(string from, string to)
+    {
+        if (!CreateHardLink(to, from, IntPtr.Zero))
+        {
+            int error = Marshal.GetLastWin32Error();
+            throw new System.ComponentModel.Win32Exception(error);
+        }
+    }
+}
