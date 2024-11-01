a9bd57874a308d7d3a72e10dea9ea8cc66b757f4
Bronuh <bronyx@yandex.ru>
2024-11-01 12:32:11 +0300

    Migration to pashtet-mc

--- LegacyLauncher/LegacyLauncher/External/ILoadable.cs
+++
@@ -1,5 +0,0 @@
-﻿
-public interface ILoadable
-{
-	void Load();
-}
--- LegacyLauncher/LegacyLauncher/Updaters/VersionInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿public class VersionInfo
-{
-	public int ClientVersion = 0, LauncherVersion = 0;
-	public string LauncherLink, ClientLink;
-}
