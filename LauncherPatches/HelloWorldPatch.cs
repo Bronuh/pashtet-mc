3d9790ce5ec65c35ee0e1dfcd78ddc2f8ad4620a
Bronuh <bronyx@yandex.ru>
2024-11-04 18:33:49 +0300

    Minor fixes

--- LauncherPatches/HelloWorldPatch.cs
+++ LauncherPatches/HelloWorldPatch.cs
@@ -1,5 +1,7 @@
 ﻿using KludgeBox;
+using KludgeBox.Events.Global;
 using PatchApi;
+using PatchApi.Events;
 
 namespace LauncherPatches;
 
@@ -8,5 +10,11 @@ public class HelloWorldPatch : LauncherPatch
     public override void Run()
     {
         Log.Info($"Hello world!");
+        EventBus.Subscribe<RunningMainTasksOnTaskManagerEvent>(OnMainTasksRun);
+    }
+
+    private static void OnMainTasksRun(RunningMainTasksOnTaskManagerEvent evt)
+    {
+        Log.Info($"Running main tasks hook");
     }
 }
