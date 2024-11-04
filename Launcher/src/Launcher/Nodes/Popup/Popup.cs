d6715a45b685694d13009eeae98da20ca0cee830
Bronuh <bronyx@yandex.ru>
2024-11-04 16:27:49 +0300

    Add Popup events and some comments

--- Launcher/src/Launcher/Nodes/Popup/Popup.cs
+++ Launcher/src/Launcher/Nodes/Popup/Popup.cs
@@ -1,3 +1,6 @@
+using KludgeBox.Events.Global;
+using PatchApi.Events;
+
 namespace Launcher.Nodes;
 
 public partial class Popup : Control
@@ -5,6 +8,8 @@ public partial class Popup : Control
 	[Export] public HBoxContainer ButtonsContainer { get; private set; }
 	[Export] public Label TitleLabel { get; private set; }
 	[Export] public Label DescriptionLabel { get; private set; }
+	
+	public PopupRequest CurrentPopupRequest { get; private set; }
 	
 	private Queue<PopupRequest> _popupRequests = new();
 	public override void _Ready()
@@ -17,6 +22,10 @@ public partial class Popup : Control
 	{
 	}
 
+	/// <summary>
+	/// Returns PopupBuilder, attached to this Popup
+	/// </summary>
+	/// <returns></returns>
 	public PopupBuilder BeginBuild()
 	{
 		var builder = new PopupBuilder(this);
@@ -24,14 +33,36 @@ public partial class Popup : Control
 		return builder;
 	}
 
+	/// <summary>
+	/// Enqueues and shows new Popup
+	/// </summary>
+	/// <param name="popup"></param>
 	public void QueuePopup(PopupRequest popup)
 	{
+		if (EventBus.PublishIsCancelled(new PopupRequestEnqueueEvent(this, popup)))
+			return;
+		
 		_popupRequests.Enqueue(popup);
 		TryShowNextPopup();
 	}
 
+	
+	public void TryShowNextPopup()
+	{
+		if (EventBus.PublishIsCancelled(new PopupTryingToShowEvent(this)))
+			return;
+		
+		if (_popupRequests.TryDequeue(out var popup))
+		{
+			ShowPopup(popup);
+		}
+	}
+
 	private void ShowPopup(PopupRequest popup)
 	{
+		if (EventBus.PublishIsCancelled(new PopupShowEvent(this, popup)))
+			return;
+		
 		TitleLabel.Text = popup.Title;
 		DescriptionLabel.Text = popup.Description;
 		
@@ -42,7 +73,7 @@ public partial class Popup : Control
 			button.Pressed += buttonRequest.Action;
 			ButtonsContainer.AddChild(button);
 		}
-		
+		CurrentPopupRequest = popup;
 		Visible = true;
 	}
 
@@ -52,17 +83,13 @@ public partial class Popup : Control
 		Clear();
 		TryShowNextPopup();
 	}
-
-	private void TryShowNextPopup()
-	{
-		if (_popupRequests.TryDequeue(out var popup))
-		{
-			ShowPopup(popup);
-		}
-	}
 	
 	private void Clear()
 	{
+		if (EventBus.PublishIsCancelled(new PopupClearingEvent(this)))
+			return;
+		
+		CurrentPopupRequest = null;
 		TitleLabel.Text = "";
 		DescriptionLabel.Text = "";
 		foreach (var button in ButtonsContainer.GetChildren())
---
+++ Launcher/src/PatchApi/Events/PopupEvents.cs
@@ -0,0 +1,10 @@
+﻿using KludgeBox.Events.EventTypes;
+using Launcher.Nodes;
+using Popup = Launcher.Nodes.Popup;
+
+namespace PatchApi.Events;
+
+public class PopupRequestEnqueueEvent(Popup Popup, PopupRequest Request) : CancellableEvent;
+public class PopupTryingToShowEvent(Popup Popup) : CancellableEvent;
+public class PopupShowEvent(Popup Popup, PopupRequest Request) : CancellableEvent;
+public class PopupClearingEvent(Popup Popup) : CancellableEvent;
