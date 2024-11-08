407fac8b999306ef9a4e52ad5a1118e883391fbf
Bronuh <bronyx@yandex.ru>
2024-11-08 16:31:03 +0300

    Add ModInfo class for optional mods

---
+++ Common/Api/ModInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Api;
+
+public class ModInfo(string readableName, string fileName, string description, Dictionary<string, object> metadata = null)
+{
+    public string FileName { get; set; } = fileName;
+    public Dictionary<string, object> Metadata { get; } = metadata;
+    public string ReadableName { get; } = readableName;
+    public string Description { get; } = description;
+
+    public override string ToString()
+    {
+        return $"{ReadableName}: {Description}";
+    }
+}
