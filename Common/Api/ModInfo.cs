851960339c5afd40a14d22e43c2e02f8335c0aca
Bronuh <bronyx@yandex.ru>
2024-11-08 18:04:30 +0300

    Add metadata property names

--- Common/Api/ModInfo.cs
+++ Common/Api/ModInfo.cs
@@ -2,6 +2,11 @@
 
 public class ModInfo(string readableName, string fileName, string description, Dictionary<string, string> metadata = null)
 {
+    public static string MetaModUrl = "modUrl";
+    public static string MetaModVersion = "modVersion";
+    public static string MetaModDownloadUrl = "downloadUrl";
+    public static string MetaModDependencies = "dependsOn";
+    
     public string ReadableName { get; } = readableName;
     public string FileName { get; set; } = fileName;
     public string Description { get; } = description;
